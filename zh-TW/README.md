115ea4d00e7719a8b7dcde4e661da791ed6afe18
akar1nchan <kawaiiushio@gmail.com>
2015-10-18 18:45:03 +0800

    add all

---
+++ zh-TW/README.md
@@ -0,0 +1,48 @@
+# Flarum 中文繁體/正體語言包（擴展）
+
+本項目是 [Flarum i18n](https://github.com/justjavac/flarum-i18n-zh) 的一部分。
+
+本擴展的簡體中文語言包由 [jsthon/Flarum-zh-CN](https://github.com/jsthon/Flarum-zh-CN) 提供。
+
+本地化擴展開發文檔已經翻譯完成：<http://justjavac.gitbooks.io/flarum/content/extend/localization.html>
+
+## 包括
+
+此擴展包含了 Flarum 的核心意見一些官方擴展的漢化：
+
+- [Flarum](https://github.com/flarum/core) 0.1.0-beta (Core)
+- [Likes](https://github.com/flarum/likes) 0.1.0-beta
+- [Lock](https://github.com/flarum/lock) 0.1.0-beta
+- [Mentions](https://github.com/flarum/mentions) 0.1.0-beta
+- [Pusher](https://github.com/flarum/pusher) 0.1.0-beta
+- [Sticky](https://github.com/flarum/sticky) 0.1.0-beta
+- [Subscriptions](https://github.com/flarum/subscriptions) 0.1.0-beta
+- [Tags](https://github.com/flarum/tags) 0.1.0-beta
+
+## 文檔
+
+### 如何下載？
+
+點擊 GitHub 的 [Download ZIP](https://github.com/akar1nchan/Flarum-Traditional-Chinese/archive/master.zip) 按鈕。
+
+### 如何安裝？
+
+1. 解壓下載完成的壓縮文件。
+
+2. 然後將解壓之後的文件放到 Flarum 根目錄的 **extensions** 目錄下。
+
+以管理員身份登陸網站，進入管理後台。通過點擊右上角的用户名，在彈出菜單中點擊 **Administration** 鏈接。
+
+在左側的導航中，點擊 **Extensions** 按鈕。這時你可以在右邊看到剛才安裝的兩個擴展**繁體中文** 與 **正體中文**。
+
+點擊**繁體中文** 或者 **正體中文** 右邊，在彈出菜單中選擇 **Enable**。
+
+**Flarum 中文繁體/正體語言包**語言包已經成功安裝。
+
+### FAQ
+
+語言包如果沒有生效，進入網站根目錄，打開 **assets** 目錄，將裏面的文件刪除（**不要**刪除 avatars 和 fonts）。
+
+## License
+
+本語言包基於 [MIT License](LICENSE) 發佈。
---
+++ zh-TW/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 正體中文
+ *
+ * @package   justjavac\Flarum\SimpleChinese
+ * @copyright Copyright (c) 2015 The justjavac Authors
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+namespace justjavac\Flarum\SimpleChinese;
+
+use Flarum\Support\Extension as BaseExtension;
+use Illuminate\Events\Dispatcher;
+use Flarum\Events\RegisterLocales;
+
+class Extension extends BaseExtension
+{
+    public function listen(Dispatcher $events)
+    {
+        $events->listen(RegisterLocales::class, function (RegisterLocales $event) {
+            $event->manager->addLocale('zh-CN', '中文简体');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/core.yml');
+            $event->manager->addJsFile('zh-CN', __DIR__.'/locale/core.js');
+            $event->manager->addConfig('zh-CN', __DIR__.'/locale/core.php');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/likes.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/lock.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/mentions.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/pusher.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/sticky.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/subscriptions.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/tags.yml');
+        });
+    }
+}
+
+return __NAMESPACE__ . '\\Extension';
